c867bd87c1b7859f11aed19c90fd4460a090f54c
katthigokul <gokulkatthi@gmail.com>
2019-07-30 19:04:36 +0530

    Created Task2 and git ignore

---
+++ src/main/java/com/stackroute/controller/AppController.java
@@ -0,0 +1,29 @@
+package com.stackroute.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+
+public class AppController {
+    @RequestMapping("/")
+    public String message() {
+        return "index";
+    }
+
+    @RequestMapping("/message")
+
+    public ModelAndView message(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        String username = httpServletRequest.getParameter("name");
+        String password=httpServletRequest.getParameter("password");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("greetings");
+        modelAndView.addObject("result", username);
+
+        return modelAndView;
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+<form action="message">
+Enter a user name:<input type="text" name="name"><br><br>
+Password:<input type="password" placeholder="password" name="password"><br>
+<input type="submit">
+</form>
+</body>
+</html>
